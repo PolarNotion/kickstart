5c1a0b2c950154eebf55f8852e75d7e9d59ea82a
morganjlopes <morgan@polarnotion.com>
2020-09-05 13:40:38 -0400

    Add common scopes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,11 @@ class User < ApplicationRecord
 
   has_one_attached :profile_photo
 
-  scope :contains, -> (q) { where("first_name ilike ? OR last_name ilike ? OR email ilike ?", "%#{q}%", "%#{q}%", "%#{q}%")}
+  scope :contains,         -> (q)        { where("first_name ilike ? OR last_name ilike ? OR email ilike ?", "%#{q}%", "%#{q}%", "%#{q}%")}
+  scope :created_in_range, -> (min, max) { where(created_at: min..max) }
+  scope :created_in_year,  -> (year)     { created_in_range(year.beginning_of_year, year.end_of_year)}
+  scope :recent,           ->            { created_in_range(30.days.ago, 0.minutes.ago)}
 
   validates :first_name, presence: true
-  validates :last_name,  presence: true 
+  validates :last_name,  presence: true
 end
