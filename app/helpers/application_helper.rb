5f556944ed8a297edc4bab0a920b339f685575d8
morganjlopes <morgan@polarnotion.com>
2020-09-05 13:06:22 -0400

    Add standard helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def format_currency(total_in_dollars, precision = 0)
+    number_to_currency(total_in_dollars, precision: precision)
+  end
+
   def format_date(datetime)
     datetime&.strftime('%b %d, %Y')
   end
@@ -17,5 +21,15 @@ module ApplicationHelper
       datetime_1&.strftime('%b %d') + ' - ' +
       format_date(datetime_2)
     end
+  end
+
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.send(association).klass.new
+    id = new_object.object_id
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render(association.to_s.singularize + "_fields", f: builder)
+    end
+    
+    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
   end
 end
